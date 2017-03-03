e9b4ae9e56576c1ab3da03b91a722570470b5dd2
Gennaro Mellone <gennaromellone@10-22-48-98.0200.par.it.iosda.org>
2017-03-03 13:32:36 +0100

    Animazione 1

---
+++ AppForeverYoung/ForeverYoungWatchKit Extension/AnimationMedicine.swift
@@ -0,0 +1,34 @@
+//
+//  AnimationMedicine.swift
+//  AppForeverYoung
+//
+//  Created by Gennaro Mellone on 03/03/17.
+//  Copyright © 2017 iosparthenopedeveloper. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class AnimationMedicine: WKInterfaceController {
+    @IBOutlet var imageObject: WKInterfaceImage!
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        imageObject.setImageNamed("antibiotic")
+        imageObject.startAnimatingWithImages(in: NSRange(location: 0, length: 13), duration: 3, repeatCount: 2)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
